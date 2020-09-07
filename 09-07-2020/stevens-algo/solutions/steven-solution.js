58e24ec2cad315a69d7f977bee7cb47415ca8091
sap2411 <steven.parsons.us@gmail.com>
2020-09-07 11:31:58 -0700

    adds stevens solution to stevens problem 09/07

--- 09-07-2020/stevens-algo/solutions/steven-solution.js
+++ 09-07-2020/stevens-algo/solutions/steven-solution.js
@@ -1,19 +1,5 @@
-var dominantIndex = function(nums) {
-    let max = {'value': -1, 'index': -1}
-    let secondMax = {'value': -1, 'index': -1}
-    for(let num in nums){
-        if(nums[num] > max['value']){
-            secondMax['value'] = max['value']
-            secondMax['index'] = max['index']
-            max['value'] = nums[num]
-            max['index'] = num
-        }else if(nums[num] > secondMax['value']){
-            secondMax['value'] = nums[num]
-            secondMax['index'] = num
-        }
-    }
-    return secondMax['value'] * 2 > max['value'] ? -1 : max['index']
-};
+//99.4% best runtime
+//24.65% best memory
 
 //create 2 objects to store a max and 2nd max value and index from the input array
 //initialize them with values that are sure to be overwritten
@@ -31,3 +17,20 @@ var dominantIndex = function(nums) {
 
 //if our max['value'] is at least twice as much as our second max, then we can return our max['index']
 //if not, we return -1
+
+var dominantIndex = function(nums) {
+    let max = {'value': -1, 'index': -1}
+    let secondMax = {'value': -1, 'index': -1}
+    for(let num in nums){
+        if(nums[num] > max['value']){
+            secondMax['value'] = max['value']
+            secondMax['index'] = max['index']
+            max['value'] = nums[num]
+            max['index'] = num
+        }else if(nums[num] > secondMax['value']){
+            secondMax['value'] = nums[num]
+            secondMax['index'] = num
+        }
+    }
+    return secondMax['value'] * 2 > max['value'] ? -1 : max['index']
+};
